ea9d15e9439a45aed5bf28446a0da250130b7c99
codeporan <codeporan@gmail.com>
2022-12-12 13:58:43 +0600

    adding templates

--- foodonline/views.py
+++ foodonline/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hello")
-    
+    return render(request,"home.html")
+
 def about(request):
     return HttpResponse("about")
